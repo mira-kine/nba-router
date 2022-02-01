f6a241347b75f7ab0857df8517ce5e51ad3ed89a
mira-kine <mirak0419@gmail.com>
2022-02-01 15:59:27 -0800

    added light styling, used useHistory on handleClick back button

--- src/components/FilmDetail/FilmDetail.jsx
+++ src/components/FilmDetail/FilmDetail.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import './FilmDetail.css';
 
-export default function FilmDetail({ title, director, image, original_title, description }) {
+export default function FilmDetail({
+  handleClick,
+  title,
+  director,
+  image,
+  original_title,
+  description,
+}) {
   return (
     <div className="movie-card">
-      <span>{title}</span>
+      <h1>{title}</h1>
+      <h2>{original_title}</h2>
       <img src={image} alt="movie image" />
-      <span>{director}</span>
-      <span>{original_title}</span>
+      <h3>{director}</h3>
       <span>{description}</span>
+      <button onClick={handleClick}>Back to List</button>
     </div>
   );
 }
--- src/views/GhibliDetail/GhibliDetail.jsx
+++ src/views/GhibliDetail/GhibliDetail.jsx
@@ -2,20 +2,32 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { fetchFilmDetails } from '../../services/ghibli';
 import FilmDetail from '../../components/FilmDetail/FilmDetail';
+import { useHistory } from 'react-router-dom';
 
 export default function GhibliDetail() {
   const [detail, setDetail] = useState({});
+  const [loading, setLoading] = useState(true);
   const { filmId } = useParams();
+  const history = useHistory();
 
   useEffect(() => {
     async function fetchDetails() {
       const filmDetails = await fetchFilmDetails(filmId);
       setDetail(filmDetails);
+      setLoading(false);
     }
     fetchDetails();
-  }, []);
+  }, [filmId]);
+
+  if (loading) {
+    return <h1>Loading</h1>;
+  }
 
   const { title, director, producer, original_title, image, description } = detail;
+  const handleClick = () => {
+    history.push(`/films`);
+  };
+
   return (
     <div>
       <FilmDetail
@@ -25,6 +37,7 @@ export default function GhibliDetail() {
         original_title={original_title}
         description={description}
         image={image}
+        handleClick={handleClick}
       />
     </div>
   );
