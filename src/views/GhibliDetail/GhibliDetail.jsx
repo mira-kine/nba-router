39033a70df6aa5ce3f2628c8159b9675e059a9d8
mira-kine <mirak0419@gmail.com>
2022-02-01 18:02:39 -0800

    history back to home page with films list

--- src/views/GhibliDetail/GhibliDetail.jsx
+++ src/views/GhibliDetail/GhibliDetail.jsx
@@ -25,7 +25,7 @@ export default function GhibliDetail() {
 
   const { title, director, producer, original_title, image, description } = detail;
   const handleClick = () => {
-    history.push(`/`);
+    history.push(`/films`);
   };
 
   return (
