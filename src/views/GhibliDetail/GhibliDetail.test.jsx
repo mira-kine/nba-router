dda9d0e1ce55c4da97fe68966a0d187fbacf8762
mira-kine <mirak0419@gmail.com>
2022-02-02 16:27:28 -0800

    cleaning up code

--- src/views/GhibliDetail/GhibliDetail.test.jsx
+++ src/views/GhibliDetail/GhibliDetail.test.jsx
@@ -51,7 +51,6 @@ test('should render Details', async () => {
 
   expect(screen.getByText(/loading/i)).toBeInTheDocument();
   const img = await screen.findByRole('img');
-
   const director = screen.getByText(/hayao miyazaki/i);
   const description = screen.getByText(
     "The orphan Sheeta inherited a mysterious crystal that links her to the mythical sky-kingdom of Laputa. With the help of resourceful Pazu and a rollicking band of sky pirates, she makes her way to the ruins of the once-great civilization. Sheeta and Pazu must outwit the evil Muska, who plans to use Laputa's science to make himself ruler of the world."
